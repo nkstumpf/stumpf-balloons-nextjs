64cc6de391edad4fdcf7cf43b6967a601fd1c195
Nick Stumpf <nick.k.stumpf@gmail.com>
2020-11-23 13:21:13 -0500

    Fixes empty react components

---
+++ pages/equipment-catalog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledSection = styled.section ``
+
+export default function Page() {
+    return (
+        <StyledSection>
+            <h1>Test Content</h1>
+        </StyledSection>
+    )
+}
