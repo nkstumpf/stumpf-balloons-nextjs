2c0c87ba82b978c471bed9df5d84d304b2f0b234
Nick Stumpf <nick.k.stumpf@gmail.com>
2021-12-23 11:57:55 -0500

    fix: remove duplicate page

--- pages/equipment-catalog/index.jsx
+++ pages/equipment-catalog/index.jsx
@@ -1,32 +1,8 @@
 import React from 'react';
 import Storyblok from '@/lib/storyblok';
-import styled from 'styled-components';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import CategoryTile from '@/components/CategoryTile';
 import Link from 'next/link';
-
-const StyledSection = styled.section `
-  margin: 0 25px;
-
-  & ul {
-    margin: 0;
-    padding: 0;
-
-    span {
-      margin-left: 10px;
-  }
-  }
-
-  & li {
-    list-style: none;
-  }
-
-  & li:hover {
-    cursor: pointer;
-    color: hotpink;
-  }
-`;
 
 const CatalogLandingPage = ({ data }) => {
   const sbPageID = 91619610;
@@ -35,7 +11,7 @@ const CatalogLandingPage = ({ data }) => {
   return (
     <>
       <Header />
-      <StyledSection>
+      <section>
           <h1>Equipment Catalog</h1>
           <h2>All Products:</h2>
             {data.map(page => {
@@ -47,8 +23,8 @@ const CatalogLandingPage = ({ data }) => {
                 );
               }
             })}
-      </StyledSection>
-      {/* <Footer /> */}
+      </section>
+      <Footer />
     </>
   );
 }
